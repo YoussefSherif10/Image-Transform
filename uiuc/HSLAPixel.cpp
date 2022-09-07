482ae22e698aca69e81cb09cfb8a40ac86798fa3
youssef fekry <yousseffekry@youssefs-MacBook-Air.local>
2022-09-07 10:21:21 +0200

    created a class called HSLAPixel within the uiuc namespace in the file uiuc/HSLAPixel.h. to initialize each pixel by four public member variables

--- uiuc/HSLAPixel.cpp
+++ uiuc/HSLAPixel.cpp
@@ -12,4 +12,6 @@
 using namespace std;
 
 namespace uiuc {
+    
+    
 }
--- uiuc/HSLAPixel.h
+++ uiuc/HSLAPixel.h
@@ -11,9 +11,10 @@
 #include <sstream>
 
 namespace uiuc {
-
-  // Put your HSLAPixel class definition here.
-  // (Remember to end it with a semicolon!)
-
+  class HSLAPixel
+  {
+  public:
+    double h,s,l,a ;
+  };
 
 }
